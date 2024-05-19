4c6329ed25007dd57df2fad1cedd713295f44ca2
yunbiyomi <yunbiyomi@naver.com>
2024-05-20 01:09:02 +0900

    ✨ Feat: node 추가 기능 추가(Front)

--- examples/api-samples/src/browser/read-model/read-model-contributions.tsx
+++ examples/api-samples/src/browser/read-model/read-model-contributions.tsx
@@ -14,11 +14,11 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { Command, CommandRegistry, MenuModelRegistry, MAIN_MENU_BAR } from '@theia/core';
+import { Command, CommandRegistry, MenuModelRegistry, MAIN_MENU_BAR, QuickInputService, QuickPickItemOrSeparator, QuickPickItem } from '@theia/core';
 import { injectable, inject, interfaces } from '@theia/core/shared/inversify';
-import { AbstractViewContribution, bindViewContribution, codicon, FrontendApplicationContribution, Widget, WidgetFactory } from '@theia/core/lib/browser';
+import { AbstractViewContribution, bindViewContribution, codicon, FrontendApplicationContribution, QuickViewService, Widget, WidgetFactory } from '@theia/core/lib/browser';
 import { ReadModelClient, ReadModel, ReadModelPath, FileNode } from '../../common/read-model/read-model-service';
-import { ReadModelWidget } from './read-model-widget';
+import { ReadModelWidget, TypeNode } from './read-model-widget';
 import { ServiceConnectionProvider } from '@theia/core/lib/browser/messaging/service-connection-provider';
 import { TabBarToolbarContribution, TabBarToolbarRegistry } from '@theia/core/lib/browser/shell/tab-bar-toolbar';
 
@@ -28,14 +28,18 @@ export const ReadModelCommand: Command = {
 };
 
 export const NodeAddToolBarCommand: Command = {
-    id: 'node-add-toolbar-command',
+    id: 'node-add-toolbar',
     iconClass: codicon('add')
-}
+};
 
 export const NodeDeleteToolBarCommand: Command = {
-    id: 'node-delete-toolbar-command',
+    id: 'node-delete-toolbar',
     iconClass: codicon('chrome-minimize')
-}
+};
+
+export const NodeQuickView: Command = {
+    id: 'node-quick-view'
+};
 
 @injectable()
 export class ReadModelFrontend implements ReadModelClient {
@@ -45,8 +49,10 @@ export class ReadModelFrontend implements ReadModelClient {
 export class ReadModelContribution extends AbstractViewContribution<ReadModelWidget> implements TabBarToolbarContribution {
 
     @inject(CommandRegistry) protected readonly commandRegistry: CommandRegistry;
-    @inject(ReadModel) protected readonly readModel: ReadModel
+    @inject(ReadModel) protected readonly readModel: ReadModel;
     @inject(ReadModelWidget) protected readonly readModelWidget: ReadModelWidget;
+    @inject(QuickViewService) protected readonly quickViewService: QuickViewService;
+    @inject(QuickInputService) protected readonly quickInputService: QuickInputService;
 
     constructor() {
         super({
@@ -85,9 +91,13 @@ export class ReadModelContribution extends AbstractViewContribution<ReadModelWid
         // Tabbar add command
         registry.registerCommand(NodeAddToolBarCommand, {
             execute: () => {
-                console.log('Add 버튼입니다.');
+                this.addFileQuickView(this.readModelWidget);
             },
-            isEnabled: widget => this.withWidget(widget, () => true),
+            isEnabled: widget => this.withWidget(widget, () => {
+                const selectNode = this.readModelWidget.model.selectedNodes[0] as TypeNode;
+                const type = selectNode?.type;
+                return type === 'model' || type === 'file';
+            }),
             isVisible: widget => this.withWidget(widget, () => true),
         });
 
@@ -98,7 +108,7 @@ export class ReadModelContribution extends AbstractViewContribution<ReadModelWid
                 this.readModelWidget.deleteNode(selectNode);
                 this.readModel.deleteNode(selectNode).then((data: string) => {
                     console.log(data);
-                })
+                });
             },
             isEnabled: widget => this.withWidget(widget, () => true),
             isVisible: widget => this.withWidget(widget, () => true),
@@ -128,7 +138,61 @@ export class ReadModelContribution extends AbstractViewContribution<ReadModelWid
         }
         return false;
     }
-}
+
+    // Quick Input 생성
+    protected async addFileQuickView(widget: ReadModelWidget | undefined): Promise<void> {
+        const items: QuickPickItemOrSeparator[] = [];
+        const selectNode = this.readModelWidget.model.selectedNodes[0] as TypeNode;
+        const type = selectNode.type;
+        const newNodeRegex = /^[A-Za-z][A-Za-z0-9_]*$/; // 정규표현식을 문자열이 아닌 정규식 리터럴로 변경
+        let quickViewTitle = 'Create';
+        let quickViewContent = 'Enter Name...';
+
+        if (widget && this.readModelWidget.model.selectedNodes) {
+            switch (type) {
+                case 'file':
+                    quickViewTitle = 'Create Model';
+                    quickViewContent = 'Enter Model Name...';
+                    break;
+                case 'model':
+                    quickViewTitle = 'Create Field';
+                    quickViewContent = 'Enter Field Name...';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        const addNewNode: QuickPickItem & { value?: string } = {
+            id: NodeQuickView.id,
+            label: 'Create New File'
+        };
+
+        this.quickInputService.showQuickPick(items, {
+            title: quickViewTitle,
+            placeholder: quickViewContent,
+            canSelectMany: false,
+            onDidChangeValue: picker => {
+                if (picker.value) {
+                    addNewNode.alwaysShow = true;
+                    addNewNode.value = picker.value;
+                    addNewNode.label = `${quickViewTitle}:  ${picker.value}`;
+
+                    if (newNodeRegex.test(picker.value)) {
+                        addNewNode.description = '';
+                        addNewNode.execute = async () => {
+                            this.readModelWidget.addNewNode(selectNode, type, addNewNode?.value);
+                        };
+                    } else {
+                        addNewNode.description = '이름 형식이 올바르지 않습니다! 다시 입력해주세요.';
+                    }
+
+                    picker.items = [...items, addNewNode];
+                }
+            }
+        });
+    }
+};
 
 export const bindReadModelWidget = (bind: interfaces.Bind) => {
     bind(ReadModelClient).to(ReadModelFrontend).inSingletonScope();
@@ -145,4 +209,4 @@ export const bindReadModelWidget = (bind: interfaces.Bind) => {
         id: ReadModelWidget.ID,
         createWidget: () => ctx.container.get<ReadModelWidget>(ReadModelWidget)
     })).inSingletonScope();
-}
+};
--- examples/api-samples/src/browser/read-model/read-model-widget.tsx
+++ examples/api-samples/src/browser/read-model/read-model-widget.tsx
@@ -16,13 +16,15 @@
 
 import * as React from '@theia/core/shared/react';
 import { Container, inject, injectable, interfaces, postConstruct } from '@theia/core/shared/inversify';
+// eslint-disable-next-line max-len
 import { codicon, CompositeTreeNode, ContextMenuRenderer, createTreeContainer, ExpandableTreeNode, LabelProvider, NodeProps, SelectableTreeNode, TreeImpl, TreeModel, TreeModelImpl, TreeNode, TreeProps, TreeWidget, URIIconReference, WidgetManager } from '@theia/core/lib/browser';
 import { FileNode, ReadModel, XmlNode } from '../../common/read-model/read-model-service';
 import { URI } from '@theia/core';
 
 export let filePath: string = '';
 
-export interface XmlTreeNode extends SelectableTreeNode, ExpandableTreeNode {
+export interface TypeNode extends SelectableTreeNode, ExpandableTreeNode {
+    type: string;
 }
 
 @injectable()
@@ -82,7 +84,7 @@ export class ReadModelWidget extends TreeWidget {
             visible: true,
             expanded: false,
             children: []
-        }
+        };
     }
 
     // 폴더 파싱한 결과를 바탕으로 폴더 및 파일 Node 생성
@@ -93,26 +95,39 @@ export class ReadModelWidget extends TreeWidget {
         const nodePath: URI = new URI(fileNode.filePath);
         const nodeType: URIIconReference = fileNode.isDirectory ? URIIconReference.create('folder', nodePath) : URIIconReference.create('file', nodePath);
 
-        const node: ExpandableTreeNode = {
+        const node: TypeNode = {
             id: fileNode.id,
             name: fileNode.id,
             icon: this.labelProvider.getIcon(nodeType),
             description: fileNode.filePath,
             expanded: false,
             parent: parent,
-            children: newChildren
-        }
+            children: newChildren,
+            type: 'file',
+            selected: false
+        };
 
         // children이 존재하는 Node의 경우
         if (fileNode.children && Array.isArray(fileNode.children)) {
+            // eslint-disable-next-line @typescript-eslint/no-shadow
+            const node: ExpandableTreeNode = {
+                id: fileNode.id,
+                name: fileNode.id,
+                icon: this.labelProvider.getIcon(nodeType),
+                description: fileNode.filePath,
+                expanded: false,
+                parent: parent,
+                children: newChildren
+            };
+
             for (const child of fileNode.children) {
                 const childNode: TreeNode = this.createTreeNode(child, node);
                 newChildren.push(childNode);
             }
-            return node
+            return node;
         }
 
-        return node
+        return node;
     }
 
     // 가져온 FileNode를 바탕으로 Node 추가
@@ -129,7 +144,7 @@ export class ReadModelWidget extends TreeWidget {
     }
 
     // xml파일을 파싱한 결과를 바탕으로 속성 Node 생성
-    protected createXmlNode(xmlNode: XmlNode, parent: XmlTreeNode): TreeNode {
+    protected createXmlNode(xmlNode: XmlNode, parent: TypeNode): TreeNode {
         const newChildren: TreeNode[] = [];
 
         const node: SelectableTreeNode = {
@@ -138,35 +153,37 @@ export class ReadModelWidget extends TreeWidget {
             icon: codicon('circle-small'),
             parent: parent,
             selected: false
-        }
+        };
 
         // children이 존재하는 Node의 경우
         if (xmlNode.children && Array.isArray(xmlNode.children)) {
-            const node: XmlTreeNode = {
+            // eslint-disable-next-line @typescript-eslint/no-shadow
+            const node: TypeNode = {
                 id: xmlNode.id as string,
                 name: xmlNode.id,
                 expanded: false,
                 parent: parent,
                 children: newChildren,
-                selected: false
-            }
+                selected: false,
+                type: 'model'
+            };
 
             for (const child of xmlNode.children) {
                 const childNode: TreeNode = this.createXmlNode(child, node);
                 newChildren.push(childNode);
             }
-            return node
+            return node;
         }
 
-        return node
+        return node;
     }
 
     // 가져온 XmlNode를 바탕으로 Node 추가
-    async getReadXml(xmlNode: XmlNode[], rootNode: XmlTreeNode): Promise<void> {
+    async getReadXml(xmlNode: XmlNode[], rootNode: TypeNode): Promise<void> {
         xmlNode.forEach((xml: XmlNode) => {
             const node = this.createXmlNode(xml, rootNode);
             CompositeTreeNode.addChild(rootNode, node);
-        })
+        });
 
         await this.model.refresh();
     }
@@ -177,7 +194,7 @@ export class ReadModelWidget extends TreeWidget {
         if (icon) {
             return <div className={icon + ' file-icon'}></div>;
         }
-        return null;
+        return undefined;
     }
 
     // 선택한 Node 삭제
@@ -187,8 +204,41 @@ export class ReadModelWidget extends TreeWidget {
 
         await this.model.refresh();
     }
-}
 
+    async addNewNode(selectNode: CompositeTreeNode, type: string, value: string | undefined): Promise<void> {
+        const root = selectNode;
+
+        // 새로운 Model을 추가할 때
+        if (type === 'file') {
+            const newnNode: TypeNode = {
+                id: value as string,
+                name: value,
+                expanded: false,
+                parent: selectNode,
+                children: [],
+                selected: false,
+                type: 'model'
+            };
+
+            CompositeTreeNode.addChild(root, newnNode);
+        }
+
+        // 새로운 Field을 추가할 때
+        if (type === 'model') {
+            const newNode: SelectableTreeNode = {
+                id: value as string,
+                name: value,
+                icon: codicon('circle-small'),
+                parent: root,
+                selected: false
+            };
+
+            CompositeTreeNode.addChild(root, newNode);
+        }
+
+        await this.model.refresh();
+    }
+}
 
 @injectable()
 export class ReadModelTreeModel extends TreeModelImpl {
@@ -198,7 +248,7 @@ export class ReadModelTreeModel extends TreeModelImpl {
     @inject(WidgetManager) protected readonly widgetManager: WidgetManager;
 
     // Node 더블 클릭시
-    protected override doOpenNode(node: XmlTreeNode): void {
+    protected override doOpenNode(node: TypeNode): void {
         super.doOpenNode(node);
 
         // Xml파일인 경우
